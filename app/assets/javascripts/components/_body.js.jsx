401c2f70cd5104d004d44c182adeb37db62b4eda
Hector Huertas <hectorhuertas@gmail.com>
2016-04-15 20:45:46 +0200

    6

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,14 @@
+var Body = React.createClass({
+  render() {
+    return (
+      <div>
+        <div>
+          <NewSkill />
+        </div>
+        <div>
+          <AllSkills />
+        </div>
+      </div>
+    )
+  }
+});
