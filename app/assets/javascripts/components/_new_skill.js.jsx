89761da66933b4e0929beea751241164a46eb203
Hector Huertas <hectorhuertas@gmail.com>
2016-04-15 21:04:54 +0200

    7

--- app/assets/javascripts/components/_new_skill.js.jsx
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -1,8 +1,23 @@
 var NewSkill = React.createClass({
+  handleClick() {
+    var name    = this.refs.name.value;
+    var details = this.refs.details.value;
+
+    $.ajax({
+      url: '/api/v1/skills',
+      type: 'POST',
+      data: { skill: { name: name, details: details } },
+      success: (skill) => {
+        this.props.handleSubmit(skill);
+      }
+    });
+  },
   render() {
     return (
       <div>
-        <h1>New skill</h1>
+        <input ref='name' placeholder='Enter name of skill' />
+        <input ref='details' placeholder='Details' />
+        <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
